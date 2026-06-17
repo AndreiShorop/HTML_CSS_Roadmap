7b2847a77efe4732c10fd9284534ca6a53f2d1a6
Andrey <ashorop6787@gmail.com>
2026-06-17 07:45:12 +0200

    merge

---
+++ HTML/script.js
@@ -0,0 +1,65 @@
+"use strict";
+// alert(
+//   "Прокрути страницу вниз, чтобы увидеть, как желтая плашка прилипает к верхнему краю окна.",
+// );
+
+// alert("Hello");
+// [1, 2].forEach(alert);
+
+// let message = "Hello";
+// alert(message);
+
+// let user = "John",
+//   age = 25,
+//   messsage = "Hello";
+// alert(user);
+// alert(age);
+// alert(messsage);
+
+// constant values
+
+// const birthday = "18.04.1982";
+
+// const age = someCode(birthday);
+
+// let name = "John";
+
+// // embed a variable
+// alert(`Hello, ${name}!`); // Hello, John!
+
+// // embed an expression
+// alert(`the result is ${1 + 2}`); // the result is 3
+
+// let nameFieldChecked = true;
+
+// typeof 0 // "number"
+
+// let result = prompt("What is the official name of JavaScript?", "");
+
+// if (result === "ECMAScript") {
+//   alert("Right!");
+// } else {
+//   alert('You don\'t know? "ECMAScript"!');
+// }
+// let isBoss = confirm("Are you the boss?");
+
+// alert(isBoss); // true if OK is pressed
+
+// let name = prompt("What is your name?", "");
+
+// alert(`Your name is ${name}`);
+
+// let value = true;
+// alert(typeof value); // boolean
+
+// value = String(value); // now value is a string "true"
+// alert(typeof value); // string
+
+let apples = "2";
+let oranges = "3";
+
+// both values converted to numbers before the binary plus
+alert(+apples + +oranges); // 5
+
+// the longer variant
+// alert( Number(apples) + Number(oranges) ); // 5
